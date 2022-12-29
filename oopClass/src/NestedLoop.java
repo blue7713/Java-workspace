649b43aac4b5c86071eb9bcd35fcd6b1bbc30444
Ryu hyeon seok <gustjr1058@naver.com>
2022-12-29 23:01:19 +0900

    Chapter3 Example

---
+++ oopClass/src/NestedLoop.java
@@ -0,0 +1,11 @@
+public class NestedLoop {
+	public static void main(String[] args) {
+		for(int i = 1; i < 10; i++) { // 1단에서 9단
+			for(int j = 1; j < 10; j++) {
+				System.out.print(i + "*" + j + "=" + i*j);
+				System.out.print('\t'); // 하나씩 탭으로 띄우기
+			}
+			System.out.println(); // 한 단이 끝나면 다음 줄로 커서 이동
+		}
+	}
+}
