2e50aaed3527e03966dad252a4ea79795e3eca9b
Ryu Hyeon Seok <gustjr1058@naver.com>
2023-01-02 20:52:50 +0900

    Chapter04 Example

---
+++ oopClass/src/ClacEx2.java
@@ -0,0 +1,19 @@
+class Clac{
+	public static int abs(int a) {
+		return a > 0 ? a : -a;
+	}
+	public static int max(int a, int b) {
+		return (a > b)? a : b;
+	}
+	public static int min(int a, int b) {
+		return (a > b)? b : a;
+	}
+}
+
+public class ClacEx2 {
+	public static void main(String[] args) {
+		System.out.println(Clac.abs(-5));
+		System.out.println(Clac.max(10, 8));
+		System.out.println(Clac.min(-3, -8));
+	}
+}
