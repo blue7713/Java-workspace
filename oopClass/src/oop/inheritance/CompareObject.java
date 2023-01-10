cef350d9bf1360db7e5a22b6ced93febbdac2b8d
Ryu Hyeon Seok <gustjr1058@naver.com>
2023-01-10 21:31:40 +0900

    OOP_05_PartA(Object)

---
+++ oopClass/src/oop/inheritance/CompareObject.java
@@ -0,0 +1,71 @@
+package oop.inheritance;
+
+public class CompareObject {
+	public static void main(String[] args) {
+		Integer x1 = 3;
+		Integer x2 = 3;
+		if(x1 == x2) {
+			System.out.println("Yes, it's the same!");
+		}
+		else {
+			System.out.println("No, it's not the same!");
+		}
+		
+		Integer y1 = new Integer(3);
+		Integer y2 = new Integer(3);
+		if(y1 == y2) {
+			System.out.println("Yes, it's the same!");
+		}
+		else {
+			System.out.println("Np, it's not the same!");
+		}
+		
+		String s1 = "Help";
+		String s2 = "Help";
+		if(s1 == s2) {
+			System.out.println("Yes, it's the same!");
+		}
+		else {
+			System.out.println("Np, it's not the same!");
+		}
+		
+		String t1 = new String("Help");
+		String t2 = new String("Help");
+		if(t1 == t2) {
+			System.out.println("Yes, it's the same!");
+		}
+		else {
+			System.out.println("No, it's not the same!");
+		}
+		
+		if(y1.equals(y2)) {
+			System.out.println("Yes, it's equal!");
+		}
+		else {
+			System.out.println("No, it's not eqaul!");
+		}
+		
+		if(t1.equals(t2)) {
+			System.out.println("Yes, it's equal!");
+		}
+		else {
+			System.out.println("No, it's not equal!");
+		}
+		
+		ValueObject v1 = new ValueObject(3, true);
+		ValueObject v2 = new ValueObject(3, true);
+		if(v1 == v2) {
+			System.out.println("Yes, it's the same!");
+		}
+		else {
+			System.out.println("No, it's not the same!");
+		}
+		
+		if(v1.equals(v2)) {
+			System.out.println("Yes, it's eqaul!");
+		}
+		else {
+			System.out.println("No, it's not equal!");
+		}
+	}
+}
---
+++ oopClass/src/oop/inheritance/ValueObject.java
@@ -0,0 +1,20 @@
+package oop.inheritance;
+
+public class ValueObject {
+	private int value;
+	private boolean flag;
+	
+	public ValueObject(int x, boolean b) {
+		this.value = x;
+		this.flag = b;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(!(obj instanceof ValueObject)) {
+			return false;
+		}
+		ValueObject that = (ValueObject) obj;
+		return this.value == that.value && this.flag == that.flag;
+	}
+}
