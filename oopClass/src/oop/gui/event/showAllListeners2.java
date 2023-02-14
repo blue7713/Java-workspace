e7dfb38540ae3ebf4e9b80eff8e51febe674080c
Ryu Hyeon Seok <gustjr1058@naver.com>
2023-02-14 14:31:36 +0900

    OOP_10_PartB(Listeners)

---
+++ oopClass/src/oop/gui/event/showAllListeners2.java
@@ -0,0 +1,40 @@
+package oop.gui.event;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class showAllListeners2 extends JFrame{
+	private JTextField name = new JTextField("JTextArea", 25);
+	private JTextArea results = new JTextArea(40, 35);
+	
+	class MyListener implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			results.append(e.getActionCommand() + "\n");
+		}
+	}
+	
+	public showAllListeners2() {
+		name.addActionListener(new MyListener());
+		JPanel top = new JPanel();
+		top.add(new JLabel("Type Swing class name (press Enter):"));
+		top.add(name);
+		add(BorderLayout.NORTH, top);
+		add(BorderLayout.CENTER, new JScrollPane(results));
+		pack();
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		name.postActionEvent();
+	}
+
+	public static void main(String[] args) {
+		new showAllListeners2();
+	}
+}
